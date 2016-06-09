f084839b91c84a02727f9021f3cd2d2a9d8b49f3
Dave Guarino <dave@codeforamerica.org>
2016-06-09 10:07:46 -0700

    IMPROVE DECIDOR

--- the_decider.rb
+++ the_decider.rb
@@ -1,8 +1,7 @@
 module TheDecider
   def self.tell_me_who_will(description)
-    team_members = %w(0 Tamara Reed Dave)
-    "#{team_members[rand(1..3)].upcase} will #{description}."
+    team_members = %w(Dave Alan Charlie Erik Andrew)
+    "#{team_members.sample.upcase} will #{description}."
   end
 end
-
 
